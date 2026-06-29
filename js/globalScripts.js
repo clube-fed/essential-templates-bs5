0dba7faed443451c3ea28c02b75142ab49b138a2
Brad Rubin <bradrfedev@gmail.com>
2026-06-29 14:07:42 -0400

    Add GK Components Meet The Team Scripts

--- js/globalScripts.js
+++ js/globalScripts.js
@@ -1295,10 +1295,12 @@ $j('.card-group.opt6-prem .card').each(function () {
 });
 */
 
+/* End Component Cards */
+
 /******************* 
 GK Component Crossmarketing panel
 ******************/
-/*
+
 $j( '.crossmarketing-panel-prem' ).each( function(){
     $j(this).append("<div class='cross-admin'><div class='cross-cte-txt'></div></div>"); 
     
@@ -1335,7 +1337,7 @@ $j( '.crossmarketing-panel-prem' ).each( function(){
     
     //$j(this).find('.container .mpContent > a').append("<span class='nc-icon-mini arrows-1_small-triangle-right'></span>");
 });
-*/
+
 
 /******************* 
 End Premiere Crossmarketing panel 
@@ -1349,8 +1351,89 @@ wow=new WOW({boxClass:'wow',animateClass:'animated',offset:0,mobile:true,live:tr
 new WOW().init();
 */
 
+/******************* 
+Begin Meet The Team v1 GK Components - change class of parent div if needed
+******************/
+$j( '.card-group.meet-the-team-1-prem .card' ).each( function(){    
+    if (!$j.trim($j('.card-text .mpContent a',this).html()).length){ 
+	   $j(this).addClass('card-no-link');
+    };
+    var callout_href = $j('.card-text .mpContent a',this).attr("href");
+    var callout_target = $j('.card-text .mpContent a',this).attr("target");
+    var outerCalloutAnchor = $j( "<div class='card-link-wrap'></div>" );
+    $j(this).wrapInner( outerCalloutAnchor );
+    $j(this).append("<div class='card-admin'><div class='card-cte-img'></div><div class='card-cte-txt'></div></div>");    
+    $j(this).find('.card-bg-img').css('background-image', "url("+ $j(this).find('.card-bg-img .mpContent img').attr("src") +")");    
+    var calloutTitle = $j.trim($j('.card-text .mpContent h4',this).text());
+    var ImageAltText = "" + calloutTitle;
+    var altAttr = $j('.card-bg-img .mpContent img',this).attr('alt');
+    $j('.card-bg-img .mpContent img',this).attr('alt',ImageAltText);
+    console.log(ImageAltText);
+    if(!$j.trim($j('.card-bg-img .mpContent',this).html()).length) { 
+	$j(this).addClass('emptyCard');
+    };
+    var imgCte = $j('.card-bg-img', this).children('.clickToEditDiv').detach();
+    var txtCte = $j('.card-text', this).children('.clickToEditDiv').detach();
+    $j(this).find('.card-admin .card-cte-img').append(imgCte);
+    $j(this).find('.card-admin .card-cte-txt').append(txtCte);
+    $j(this).find('.figure-bg-img .mpContent').contents().unwrap();
+    //$j(this).find('.card-text .mpContent a').contents().unwrap();
+});
+/******************* 
+End Meet The Team v1 
+******************/
 
-/* End Component Cards */
+/******************* 
+Begin Meet The Team v2 GK Components
+******************/
+$j( '.card-group.meet-the-team-2-prem .card' ).each( function(){    
+    if (!$j.trim($j('.card-text .mpContent a',this).html()).length){ 
+	   $j(this).addClass('card-no-link');
+    };
+    var callout_href = $j('.card-text .mpContent a',this).attr("href");
+    var callout_target = $j('.card-text .mpContent a',this).attr("target");
+    var outerCalloutAnchor = $j( "<div class='card-link-wrap'></div>" );
+    $j(this).wrapInner( outerCalloutAnchor );
+    $j(this).append("<div class='card-admin'><div class='card-cte-img'></div><div class='card-cte-name'></div><div class='card-cte-txt'></div></div>");    
+    $j(this).find('.card-bg-img .mpContent').css('background-image', "url("+ $j(this).find('.card-bg-img .mpContent img').attr("src") +")");    
+    var calloutTitle = $j.trim($j('.card-text .mpContent h4',this).text());
+    var ImageAltText = "" + calloutTitle;
+    var altAttr = $j('.card-bg-img .mpContent img',this).attr('alt');
+    $j('.card-bg-img .mpContent img',this).attr('alt',ImageAltText);
+    console.log(ImageAltText);
+    if(!$j.trim($j('.card-bg-img .mpContent',this).html()).length) { 
+	$j(this).addClass('emptyCard');
+    };
+    var imgCte = $j('.card-bg-img', this).children('.clickToEditDiv').detach();
+    var nameCte = $j('.card-name', this).children('.clickToEditDiv').detach();
+    var txtCte = $j('.card-text', this).children('.clickToEditDiv').detach();
+    $j(this).find('.card-admin .card-cte-img').append(imgCte);
+    $j(this).find('.card-admin .card-cte-name').append(nameCte);
+    $j(this).find('.card-admin .card-cte-txt').append(txtCte);
+    $j(this).find('.figure-bg-img .mpContent').contents().unwrap();
+    //$j(this).find('.card-text .mpContent a').contents().unwrap();
+    $j(this).find('.card-text .mpContent a[href*="mail"]').prepend("<span class='nc-icon-outline ui-1_email-83'></span>");
+    $j(this).find('.card-text .mpContent a[href*="tel"]').prepend("<span class='nc-icon-outline ui-3_phone'></span>");
+});
+/******************* 
+End Meet The Team v2 GK Components
+******************/
+
+/******************* 
+Begin Meet The Team v3 
+******************/
+$j( '.card-group.meet-the-team-3-prem .card' ).each( function(){
+    $j(this).find('.card-body .mpContent a[href*="mail"]').prepend("<span class='nc-icon-outline ui-1_email-83'></span>");
+    $j(this).find('.card-body .mpContent a[href*="tel"]').prepend("<span class='nc-icon-outline ui-3_phone'></span>");
+});
+/******************* 
+End Meet The Team v3 
+******************/
+//formbase edits
+//$j('.formBaseFormWrapper > div:eq(1)').addClass('formBtn');
+//$j('.formBaseFormWrapper > div:eq(1)').addClass('clearFix');
+//$j('.formBaseFormWrapper > div:eq(1) > div:eq(1)').addClass('secureLink');
+
 
 //formbase edits
 //$j('.formBaseFormWrapper > div:eq(1)').addClass('formBtn');
